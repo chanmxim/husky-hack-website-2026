dd7a8c89319e875a8c7760b669078770651783a4
Sokmontrey <sythatsokmontrey@gmail.com>
2026-02-10 19:54:49 -0500

    feat: add default profile SVG and update TeamMemberPhoto component to use it

--- src/components/team/TeamMemberPhoto.tsx
+++ src/components/team/TeamMemberPhoto.tsx
@@ -9,7 +9,7 @@ export default function TeamMemberPhoto({ mainProfilePicturePath, secondaryProfi
     return (
         <>
             {mainProfilePicturePath || secondaryProfilePicturePath ? (
-                <div className={`relative bg-gray-200 overflow-hidden rounded-full ${className}`}>
+                <div className={`relative bg-gray-200 overflow-hidden rounded-3xl ${className}`}>
                     <div
                         className="absolute inset-0 bg-cover bg-center transition-opacity duration-300"
                         style={{
@@ -26,10 +26,12 @@ export default function TeamMemberPhoto({ mainProfilePicturePath, secondaryProfi
                     )}
                 </div>
             ) : (
-                <div className={`bg-gray-100 flex items-center justify-center rounded-full ${className}`}>
-                    <svg className="w-8 h-8 md:w-10 md:h-10 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-                    </svg>
+                <div className={`bg-neutral-700 flex items-center justify-center rounded-3xl ${className} relative`}>
+                    <img
+                        src="/team-profiles/default-profile.svg"
+                        alt="Default Profile"
+                        className="w-16 h-16 object-contain"
+                    />
                 </div>
             )}
         </>
